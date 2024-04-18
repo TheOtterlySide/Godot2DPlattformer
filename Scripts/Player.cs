f1f161ed82ef8d90a34687922fbf52bb02f0d23b
The Otterly Side <47543964+TheOtterlySide@users.noreply.github.com>
2024-04-18 10:41:56 +0200

    add: Player Movement

---
+++ Scripts/Player.cs
@@ -0,0 +1,30 @@
+using Godot;
+using System;
+
+public partial class Player : CharacterBody2D
+{
+    private float Speed = 100f;
+    private float JumpForce = 200f;
+    private float Gravity = 500f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        Vector2 velocity = Velocity;
+
+        // Add the gravity.
+        velocity.Y += Gravity * (float)delta;
+
+        // Handle jump.
+        if (Input.IsActionJustPressed("Jump") && IsOnFloor())
+        {
+            velocity.Y = -JumpForce;
+        }
+
+        // Get the input direction.
+        float direction = Input.GetAxis("MoveLeft", "MoveRight");
+        velocity.X = direction * Speed;
+
+        Velocity = velocity;
+        MoveAndSlide();
+    }
+}
